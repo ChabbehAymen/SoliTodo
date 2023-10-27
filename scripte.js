8edc43c157040a7d1938789645b17e4a3f683f48
devaymen15.5.3@gmail.com <devaymen15.5.3@gmail.com>
2023-10-27 20:55:25 +0100

    modified:   scripte.js

--- scripte.js
+++ scripte.js
@@ -1,27 +1,67 @@
+let todoItemContainer = document.createElement("div");
+todoItemContainer.classList.add("todo-item");
+todoItemContainer.appendChild(itemHeader);
 
+// task body
 
+todoItemContainer.appendChild(discHolder);
+
+// task btns
+
+todoItemContainer.appendChild(taskBtnsContainer);
+let todosContainer = document.querySelector(".all-todos");
+todosContainer.appendChild(todoItemContainer);
 
 function createTask() {
-    let todoItemContainer = document.createElement('div').classList.add('todo-item');
-    let itemHeader = document.createElement('div').classList.add('item-header');
-    itemHeader.appendChild(document.createElement('hr'));
-    let itemDetails = document.createElement('div').classList.add('item-details');
-    // task title
-    let taskTitelText = document.createTextNode('Complete Html');
-    let taskTitle = document.createElement('h3');
-    taskTitle.appendChild(taskTitelText);
-    //task Date
-    let detailsContainer = document.createElement('div').classList.add('details');
-    let detailsText = document.createTextNode('15-10-2023 | 15-10-2023');
-    let createDetails = document.createElement('h4');
-    createDetails.appendChild(detailsText);
-    // task body
-    let taskDiscText = document.createTextNode('Lorem ipsum dolor, sit amet consectetur adipisicing elit.');
-    let discHolder = document.createElement('p');
-    discHolder.appendChild(taskDiscText);
-    // task btns
-    
+  let todoItemContainer = document.createElement("div");
+  todoItemContainer.classList.add("todo-item");
+}
 
+function crateItemHeader() {
+  let itemHeader = document.createElement("div");
+  itemHeader.classList.add("item-header");
+  let indicator = document.createElement("hr");
+  itemHeader.appendChild(indicator);
 
+  let itemDetails = document.createElement("div");
+  itemDetails.classList.add("item-details");
+  itemHeader.appendChild(itemDetails);
 
+  // task title
+  let taskTitelText = document.createTextNode("Complete Html");
+  let taskTitle = document.createElement("h3");
+  taskTitle.appendChild(taskTitelText);
+  itemDetails.appendChild(taskTitle);
+
+  //task Date
+  let detailsText = document.createTextNode("15-10-2023 | 15-10-2023");
+  let createDetails = document.createElement("h4");
+  createDetails.appendChild(detailsText);
+  itemDetails.appendChild(createDetails);
+  return itemHeader;
+}
+
+function craeteItemDiscreption() {
+  let taskDiscText = document.createTextNode(
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+  );
+  let discHolder = document.createElement("p");
+  discHolder.appendChild(taskDiscText);
+  return discHolder;
+}
+
+function createTaskBtns() {
+  let taskBtnsContainer = document.createElement("div");
+  taskBtnsContainer.classList.add("items-btns");
+  let cancelBtn = document.createElement("button");
+  cancelBtn.classList.add("cancel-btn");
+  cancelBtn.textContent = "Cancel";
+
+  let completeBtn = document.createElement("button");
+  completeBtn.classList.add("complete-btn");
+  completeBtn.textContent = "Complete";
+
+  taskBtnsContainer.appendChild(cancelBtn);
+  taskBtnsContainer.appendChild(completeBtn);
+  return taskBtnsContainer;
 }
