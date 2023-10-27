e3fef1943c352b4d42d54bf648a888f483c2fffb
ChabbehAymen <chebbehaymen@gmail.com>
2023-10-27 15:59:34 +0100

    modified:   style.css

--- scripte.js
+++ scripte.js
@@ -1,10 +1,1 @@
 
-
-// document.querySelectorAll('.todo-item').forEach(item =>{
-//     item.addEventListener('mouseover', ()=>{
-//         item.childNodes[3].classList.toggle('show-disc');
-//     })
-//     item.addEventListener('mouseout', ()=>{
-//         item.childNodes[3].classList.toggle('show-disc');
-//     })
-// })
